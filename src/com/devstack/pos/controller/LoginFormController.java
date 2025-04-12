e5cdef0b42da2dc1ed7f030a04f35a274d85df7a
Adeesha Sandun <112515203+Adeesha1997@users.noreply.github.com>
2025-04-12 23:50:16 +0530

    Form Changers

--- src/com/devstack/pos/controller/LoginFormController.java
+++ src/com/devstack/pos/controller/LoginFormController.java
@@ -1,4 +1,17 @@
 package com.devstack.pos.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXTextField txtPassword;
+
+    public void btnSigningOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/com/devstack/pos/controller/SignupFormController.java
+++ src/com/devstack/pos/controller/SignupFormController.java
@@ -1,4 +1,17 @@
 package com.devstack.pos.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class SignupFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXTextField txtPassword;
+
+    public void btnRegisterOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAlreadyHaveAnAccountOnAction(ActionEvent actionEvent) {
+    }
 }
