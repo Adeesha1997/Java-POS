12763274c1701615954a7d6d312cb64d49f4c3ac
Adeesha Sandun <112515203+Adeesha1997@users.noreply.github.com>
2025-04-13 01:24:50 +0530

    Login & Registration Completed

--- src/com/devstack/pos/controller/LoginFormController.java
+++ src/com/devstack/pos/controller/LoginFormController.java
@@ -1,17 +1,69 @@
 package com.devstack.pos.controller;
 
+import com.devstack.pos.util.PasswordManager;
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+import java.sql.*;
 
 public class LoginFormController {
     public AnchorPane context;
     public JFXTextField txtEmail;
-    public JFXTextField txtPassword;
+    public JFXPasswordField txtPassword;
 
-    public void btnSigningOnAction(ActionEvent actionEvent) {
+    public void btnSigningOnAction(ActionEvent actionEvent) throws IOException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mySql://localhost:3306/robotikka", "root", "1234");
+
+
+            String sql = "SELECT * FROM user WHERE email=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, txtEmail.getText());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()){
+                if (PasswordManager.checkPassword(txtPassword.getText(), resultSet.getString("password"))){
+                    System.out.println("Completed");
+                }else {
+                    new Alert(Alert.AlertType.WARNING, "Check Your Password And Try Again !").show();
+                }
+            }else {
+                new Alert(Alert.AlertType.WARNING, "User Email Not Found Try Again !").show();
+            }
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+
     }
 
-    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) {
+    private void clearFiles() {
+        txtEmail.clear();
+        txtPassword.clear();
     }
+
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignupForm");
+    }
+
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml")))
+        );
+        stage.centerOnScreen();
+    }
+
 }
