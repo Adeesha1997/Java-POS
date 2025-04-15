85f60ff91fba5021bbfa489562cb05910947c974
Adeesha Sandun <112515203+Adeesha1997@users.noreply.github.com>
2025-04-15 18:06:11 +0530

    Customer Function Setup

--- src/com/devstack/pos/dao/DatabaseAccessCode.java
+++ src/com/devstack/pos/dao/DatabaseAccessCode.java
@@ -1,11 +1,14 @@
 package com.devstack.pos.dao;
 
 import com.devstack.pos.dto.UserDto;
+import com.devstack.pos.dto.dto.CustomerDto;
 import com.devstack.pos.util.PasswordManager;
 
 import java.sql.*;
+import java.util.List;
 
 public class DatabaseAccessCode {
+     //=====User Management==============
     public static boolean creteUser(String email, String password) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mySql://localhost:3306/robotikka", "root", "1234");
@@ -17,7 +20,6 @@ public class DatabaseAccessCode {
 
         return preparedStatement.executeUpdate() > 0;
     }
-
 
     public static UserDto findUser(String email) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -38,5 +40,32 @@ public class DatabaseAccessCode {
         }
         return null;
     }
+    //=====User Management==============
+
+    //=====Customer Management==========
+    public static boolean createCustomer(String email, String name, String contact, double salary){
+        return false;
+    }
+
+    public static boolean updateCustomer(String email, String name, String contact, double salary){
+        return false;
+    }
+
+    public static CustomerDto findCustomer(String email){
+        return null;
+    }
+
+    public static boolean deleteCustomer(String email){
+        return false;
+    }
+
+    public static List<CustomerDto> findAllCustomer(){
+        return null;
+    }
+
+    public static List<CustomerDto> searchCustomer(String searchText){
+        return null;
+    }
+    //=====Customer Management==========
 
 }
