03bf559111415b828eee62f5382a2ac62c0ea95c
Adeesha Sandun <112515203+Adeesha1997@users.noreply.github.com>
2025-04-16 23:49:43 +0530

    Deleted Function Issue Fixed

--- src/com/devstack/pos/controller/CustomerFormController.java
+++ src/com/devstack/pos/controller/CustomerFormController.java
@@ -91,7 +91,7 @@ public class CustomerFormController {
                 try {
                     Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are You Sure ?", ButtonType.YES, ButtonType.NO);
                     Optional<ButtonType> selectedButtonType = alert.showAndWait();
-                    if (selectedButtonType.equals(ButtonType.YES)) {
+                    if (selectedButtonType.get().equals(ButtonType.YES)) {
 
                         if (DatabaseAccessCode.deleteCustomer(
                                 dto.getEmail()
