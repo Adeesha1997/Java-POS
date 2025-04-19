70198da57661118c903eff269a9342f3aeff80f8
Adeesha Sandun <112515203+Adeesha1997@users.noreply.github.com>
2025-04-20 01:00:19 +0530

    Implement Layered Architecture - DataAccessCode Setup

--- src/com/devstack/pos/dao/DatabaseAccessCode.java
+++ src/com/devstack/pos/dao/DatabaseAccessCode.java
@@ -1,10 +1,135 @@
 package com.devstack.pos.dao;
 
+import com.devstack.pos.dao.custom.CustomerDao;
+import com.devstack.pos.dao.custom.ProductDao;
+import com.devstack.pos.dao.custom.UserDao;
+import com.devstack.pos.dao.custom.impl.CustomerDaoImpl;
+import com.devstack.pos.dao.custom.impl.ProductDaoImpl;
+import com.devstack.pos.dao.custom.impl.UserDaoImpl;
 import com.devstack.pos.db.DbConnection;
+import com.devstack.pos.dto.UserDto;
+import com.devstack.pos.dto.dto.CustomerDto;
+import com.devstack.pos.entity.Customer;
+import com.devstack.pos.entity.Product;
+import com.devstack.pos.entity.User;
+import com.devstack.pos.util.PasswordManager;
 
 import java.sql.*;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseAccessCode {
+
+    UserDao userDao = new UserDaoImpl();
+    CustomerDao customerDao = new CustomerDaoImpl();
+    ProductDao productDao = new ProductDaoImpl();
+
+
+    //=====User Management==============
+    public boolean creteUser(String email, String password) throws ClassNotFoundException, SQLException {
+
+        return userDao.saveUser(
+                new User(email, password)
+        );
+    }
+
+    public UserDto findUser(String email) throws ClassNotFoundException, SQLException {
+
+        User user = userDao.findUser(email);
+
+        if (user != null) {
+            return new UserDto(
+                    user.getEmail(),
+                    user.getPassword()
+            );
+        }
+        return null;
+    }
+    //=====User Management==============
+
+
+    //=====Customer Management==========
+    public boolean createCustomer(String email, String name, String contact, double salary) throws SQLException, ClassNotFoundException {
+
+        return customerDao.saveCustomer(
+                new Customer(email, name, contact, salary)
+        );
+    }
+
+    public boolean updateCustomer(String email, String name, String contact, double salary) throws ClassNotFoundException, SQLException {
+
+        return customerDao.updateCustomer(
+                new Customer(email, name, contact, salary)
+        );
+    }
+
+    public CustomerDto findCustomer(String email) throws ClassNotFoundException, SQLException {
+
+        Customer customer = customerDao.findCustomer(email);
+        if (customer != null) {
+            return new CustomerDto(
+                    customer.getEmail(),
+                    customer.getName(),
+                    customer.getContact(),
+                    customer.getSalary()
+            );
+        }
+        return null;
+    }
+
+    public boolean deleteCustomer(String email) throws ClassNotFoundException, SQLException {
+
+        return customerDao.deleteCustomer(email);
+    }
+
+    public List<CustomerDto> findAllCustomer() throws ClassNotFoundException, SQLException {
+
+        List<CustomerDto> dtos = new ArrayList<>();
+        for (Customer c : customerDao.findAllCustomer()
+        ) {
+            dtos.add(new CustomerDto(
+                    c.getEmail(),
+                    c.getName(),
+                    c.getContact(),
+                    c.getSalary()
+            ));
+        }
+        return dtos;
+    }
+
+    public List<CustomerDto> searchCustomer(String searchText) throws ClassNotFoundException, SQLException {
+
+        searchText = "%" + searchText + "%";
+
+        List<CustomerDto> dtos = new ArrayList<>();
+        for (Customer c : customerDao.searchCustomer(searchText)
+        ) {
+            dtos.add(new CustomerDto(
+                    c.getEmail(),
+                    c.getName(),
+                    c.getContact(),
+                    c.getSalary()
+            ));
+        }
+        return dtos;
+
+    }
+    //=====Customer Management==========
+
+    //=====Product Management==========
+    public int getLastProductId() throws SQLException, ClassNotFoundException {
+
+        return productDao.getLastProductId();
+    }
+
+    public boolean createProduct(int code, String description) throws SQLException, ClassNotFoundException {
+
+        return productDao.saveProduct(
+                new Product(code, description)
+        );
+    }
+
+    //=====Product Management==========
+
 
 }
