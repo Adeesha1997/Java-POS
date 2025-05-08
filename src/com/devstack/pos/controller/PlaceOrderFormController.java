38bb8d1672738201a55a9a0653e473c8c7ce344d
Adeesha Sandun <112515203+Adeesha1997@users.noreply.github.com>
2025-05-08 23:57:54 +0530

    Cart Table Setup

--- src/com/devstack/pos/controller/PlaceOrderFormController.java
+++ src/com/devstack/pos/controller/PlaceOrderFormController.java
@@ -6,13 +6,15 @@ import com.devstack.pos.bo.custom.ProductDetailBo;
 import com.devstack.pos.dto.CustomerDto;
 import com.devstack.pos.dto.ProductDetailJoinDto;
 import com.devstack.pos.enums.BoType;
+import com.devstack.pos.view.tm.CartTm;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -40,30 +42,49 @@ public class PlaceOrderFormController {
 
     private final CustomerBo bo = BoFactory.getInstance().getBo(BoType.CUSTOMER);
     private final ProductDetailBo productDetailBo = BoFactory.getInstance().getBo(BoType.PRODUCT_DETAIL);
+    public TableView<CartTm> tblCart;
+    public TableColumn colCode;
+    public TableColumn colDescription;
+    public TableColumn colSellingPrice;
+    public TableColumn colDiscount;
+    public TableColumn colShowPrice;
+    public TableColumn colQty;
+    public TableColumn colTotalPrice;
+    public TableColumn colOperation;
 
 
+    public void initialize(){
+        colCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        colSellingPrice.setCellValueFactory(new PropertyValueFactory<>("sellingPrice"));
+        colDiscount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+        colShowPrice.setCellValueFactory(new PropertyValueFactory<>("showPrice"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colTotalPrice.setCellValueFactory(new PropertyValueFactory<>("totalCost"));
+        colOperation.setCellValueFactory(new PropertyValueFactory<>("btn"));
+    }
     public void btnAddNewCustomerOnAction(ActionEvent actionEvent) throws IOException {
-        setUi("CustomerForm",true);
+        setUi("CustomerForm", true);
     }
 
     public void btnAddNewProductOnAction(ActionEvent actionEvent) throws IOException {
-        setUi("ProductMainForm",true);
+        setUi("ProductMainForm", true);
     }
 
     public void btnBackToHomeOnAction(ActionEvent actionEvent) throws IOException {
-        setUi("DashboardForm",false);
+        setUi("DashboardForm", false);
     }
 
 
-    private void setUi(String url,boolean state) throws IOException {
+    private void setUi(String url, boolean state) throws IOException {
         Stage stage = null;
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("../view/" + url + ".fxml")));
 
-        if (state){
+        if (state) {
             stage = new Stage();
             stage.setScene(scene);
             stage.show();
-        }else {
+        } else {
             stage = (Stage) context.getScene().getWindow();
             stage.setScene(scene);
             stage.centerOnScreen();
@@ -75,13 +96,13 @@ public class PlaceOrderFormController {
         try {
 
             CustomerDto customer = bo.findCustomer(txtEmail.getText());
-            if (customer!=null){
+            if (customer != null) {
                 txtName.setText(customer.getName());
                 txtContact.setText(customer.getContact());
                 txtSalary.setText(String.valueOf(customer.getName()));
 
                 fetchLoyaltyCardData(txtEmail.getText());
-            }else {
+            } else {
                 new Alert(Alert.AlertType.WARNING, "Can't Find Customer !").show();
             }
 
@@ -104,10 +125,10 @@ public class PlaceOrderFormController {
     public void lodeProductOnAction(ActionEvent actionEvent) {
 
         try {
-            ProductDetailJoinDto  p= productDetailBo.findProductJoinDetails(
+            ProductDetailJoinDto p = productDetailBo.findProductJoinDetails(
                     txtBarcode.getText()
             );
-            if (p != null){
+            if (p != null) {
                 txtDescription.setText(p.getDescription());
                 txtSelllingPrice.setText(String.valueOf(p.getDto().getSellingPrice()));
                 txtDiscount.setText(String.valueOf(0));
@@ -117,7 +138,7 @@ public class PlaceOrderFormController {
 
                 txtQty.requestFocus();
 
-            }else {
+            } else {
 
                 new Alert(Alert.AlertType.WARNING, "Can't Find the Product !").show();
 
@@ -127,5 +148,28 @@ public class PlaceOrderFormController {
         }
 
 
+    }
+
+    ObservableList<CartTm> tms = FXCollections.observableArrayList();
+    public void addToCartOnAction(ActionEvent actionEvent) {
+
+
+        int qty = Integer.parseInt(txtQty.getText());
+        double sellingPrice = Double.parseDouble(txtSelllingPrice.getText());
+        double totalCost = qty * sellingPrice;
+        Button btn = new Button("Remove");
+
+        CartTm tm = new CartTm(
+                txtBarcode.getText(),
+                txtDescription.getText(),
+                sellingPrice,
+                Double.parseDouble(txtDiscount.getText()),
+                Double.parseDouble(txtShowPrice.getText()),
+                qty,
+                totalCost,
+                btn);
+
+        tms.add(tm);
+tblCart.setItems(tms);
     }
 }
